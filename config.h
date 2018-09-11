bc49320a3ccadd2c857367ee3370d8e0aad3d938
userWayneCampbell <wcampbell1995@gmail.com>
2018-09-11 13:02:03 -0400

    Add Solarized, and cool message patch, and Fedora

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "#002b36",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
