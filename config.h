97db0147732dd03332e0025d91f35b4896c16a08
userWayneCampbell <wcampbell1995@gmail.com>
2019-08-12 16:07:36 -0400

    Fix config file
    Signed-off-by: userWayneCampbell <wcampbell1995@gmail.com>

--- config.h
+++ config.h
@@ -3,16 +3,16 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#002b36",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+    [INIT] =   "#002b36",     /* after initialization */
+    [INPUT] =  "#005577",   /* during input */
+    [FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux...";
 
 /* text color */
 static const char * text_color = "#ffffff";
